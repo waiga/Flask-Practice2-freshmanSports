f17d885150242ec1779069227287d4900e3de732
wai? <84059031+waiga@users.noreply.github.com>
2023-01-02 02:21:14 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    return render_template("success.html")
+
+app.run(debug=True)
